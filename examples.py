3ae3657c862d6ca555acbc1e960425a74461f92a
VP1147 <viniciuspavao2004@gmail.com>
2019-12-02 13:29:19 -0400

    Added zoom factor
    Added zoom factor;
    Some optimizations.

--- examples.py
+++ examples.py
@@ -7,25 +7,25 @@ clock = pygame.time.Clock()
 
 x = 700 # Window heigh
 y = 700 # Window width
-Zoom = 1 # Graphic zoom STILL IN DEV
+f = 0.1 # Zoom factor
 
-def Log(x,Zoom):
+def Log(x):
 	return math.sqrt(x)
 
-def Arit(x,Zoom):
-	return (2*x)/Zoom
+def Arit(x):
+	return (2*x)
 
-def Sin(x,Zoom):
-	return math.sin(x)
+def Sin(x):
+	return math.sin(x)*20
 
-def Exp(x,Zoom):
+def Exp(x):
 	return (2**x)
 
 tg.Start(x,y,(255,255,255))
 
-tg.DrawGraph(Log,x,y,Zoom,(255,0,0))
-tg.DrawGraph(Arit,x,y,Zoom,(0,255,0))
-tg.DrawGraph(Exp,x,y,Zoom,(0,0,255))
-tg.DrawGraph(Sin,x,y,Zoom,(0,255,255))
+#tg.DrawGraph(Log,x,y,f,(255,0,0))
+#tg.DrawGraph(Arit,x,y,f,(0,255,0))
+#tg.DrawGraph(Exp,x,y,f,(0,255,255))
+tg.DrawGraph(Sin,x,y,f,(0,255,100))
 getch()
 
--- teenygraph.py
+++ teenygraph.py
@@ -5,23 +5,26 @@ import pygame
 
 clock = pygame.time.Clock()
 
-def DrawGraph(Fx,SX,SY,Zoom,rgb):
-	Px = int((SX/2)*-1)
+def DrawGraph(Fx,SX,SY,Factor,rgb):
+	Count = ((SX/2)*-1)*Factor
 	for i in range (1,SX):
 		try:
-			if SX*-2 < ((SX/2)-Fx(Px,Zoom)) < SX*2:
-				print(Fx(Px,Zoom))
-				pygame.draw.line(screen, (rgb[0], rgb[1], rgb[2]),[i, (SX/2)-Fx(Px,Zoom)],[i, ((SX+1)/2)-Fx(Px+1,Zoom)],1)
+			if SX*-10 < (SX/2)-Fx(Count) < SX*10:
+				print(Fx(Count))
+				Actual = [i, ((SX/2)-Fx(Count))] # Line start
+				Next = [i+1, (((SX+1)/2)-Fx(Count+(1*Factor)))] # Line end
+				pygame.draw.aaline(screen,rgb,Actual,Next,10)
 			else: pass
 		except ValueError: pass
-		Px += 1
+		except OverflowError: pass
+		Count += Factor
 	pygame.display.flip()
 
 def Start(SX,SY,rgb):
 	pygame.init()
 	global screen
 	screen = pygame.display.set_mode([SX,SY])
-	for i in range (1,SX): pygame.draw.rect(screen, (rgb[0], rgb[1], rgb[2]), [i, SX/2, 1, 1])
-	for i in range (1,SY): pygame.draw.rect(screen, (rgb[0], rgb[1], rgb[2]), [SY/2, i, 1, 1])
+	pygame.draw.aaline(screen,rgb,(SX/2,1),(SX/2,SX))
+	pygame.draw.aaline(screen,rgb,(SY,SY/2),(1,SY/2),1)
 	pygame.display.flip()
 
