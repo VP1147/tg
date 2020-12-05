f7caa7e414b92ced5ebcca1a99aeb0771b3b3af8
Vinícius Pavão <pavao364@gmail.com>
2020-12-05 15:02:29 -0400

    Update ex.py

--- ex.py
+++ ex.py
@@ -7,32 +7,33 @@ x = 1024	# Window width
 xs = 10
 g = 1		# Distance between grid lines
 
-
-def Log(x):
+### Math functions
+def Log(x): # f(x) = log x
 	return math.log(x,10)
 
-def Arit(x):
+def Arit(x): # f(x) = 2x
 	return 2*x
 
-def Sin(x):
+def Sin(x): # f(x) = sin(x)
 	return math.sin(x)
 
-def Exp(x):
+def Exp(x): # f(x) = 2^x
 	return 2**x
 
-def Mod(x):
+def Mod(x): # f(x) = |x|
 	return math.sqrt(x**2)
 
-def Quad(x):
+def Quad(x): # f(x) = x²
 	return x**2
+###
 
 tg.init(x,xs,g)
 
-#tg.plot(Quad)	# Quadratic
-tg.plot(Sin)	# Sinusoid
-#tg.plot(Log)	# Logarithmic
-#tg.plot(Arit)	# Arithmetic
-#tg.plot(Exp)	# Exponential
-#tg.plot(Mod)	# Modular
+tg.plot(Quad)
+tg.plot(Sin)
+tg.plot(Log)
+tg.plot(Arit)
+tg.plot(Exp)
+tg.plot(Mod)
 
 getch()
