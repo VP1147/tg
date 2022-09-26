461a9681326211887703f44ab6809e45c42fca60
Vinícius Pavão <viniciuspavao2004@gmail.com>
2022-09-26 17:40:02 -0400

    Added multiple lines option

--- calculus.py
+++ calculus.py
@@ -10,7 +10,7 @@ r = 7									# Window/Graph ratio(ratio = 10^r)
 xs = x/(2**r)							# Graph width
 g = 1									# Distance between grid lines
 
-tg.Mkrs = [0.5]
+#tg.Mkrs = [0.5]
 
 
 tg.theme("dark.json")					# Select color scheme
@@ -22,16 +22,27 @@ tg.init(x,xs,g) 						# Call the init function
 
 										# Function: f(x) = 2x²
 def f(x):
-	return 2*x**2
+	return x**2 -2*x
 
+global k
 def df(x): 								# df/dx
 	h = 0.0001							# lim x->0 
-	a = 0.5
-	m = (f(a+h)-f(a))/h
-	return m*(x-a) + f(a)	    		# y = m(x-a) + f(a)
+	a = k
+	return (f(a+h)-f(a))/h
+	
 
+def tangent(x):							# # y = m(x-a) + f(a)
+	m = df(x)
+	a = k
+	return m*(x-a) + f(a)
+
+nint = 0.5 								# Interval between two lines
+num = 10								# Number of lines (minus h-assintotes)
 tg.plot(f)
-tg.plot(df, [-100, 100, 0])
+for i in range(-int(num/2)-1, int(num/2)):
+	k = i*nint
+	tg.plot(tangent, [1, 100, 100])		# Tip: Varies the colour for each tangent line
+	#tg.clear()
 
 
 getch() 			 					# Wait for key event
